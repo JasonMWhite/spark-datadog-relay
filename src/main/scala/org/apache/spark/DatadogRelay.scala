dc0302f9561c8c4a018de7f25bad7d1a262bf85c
Jason White <jason.white@shopify.com>
2015-10-05 23:50:25 -0400

    don't throw an exception if you can't connect to statsd server

--- src/main/scala/org/apache/spark/DatadogRelay.scala
+++ src/main/scala/org/apache/spark/DatadogRelay.scala
@@ -11,15 +11,22 @@ import org.apache.spark.scheduler.{SparkListenerExecutorMetricsUpdate, SparkList
 import org.apache.spark.util.{Utils, JsonProtocol}
 import org.json4s.JsonAST.{JObject, JNothing, JValue}
 
-import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.{NonBlockingStatsDClient, StatsDClientException}
 import org.apache.spark.scheduler._
 import org.apache.spark.executor._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class DatadogRelay(conf: SparkConf) extends SparkFirehoseListener {
-  val statsd: NonBlockingStatsDClient = new NonBlockingStatsDClient("spark", "localhost", 8125)
+  val statsdOption: Option[NonBlockingStatsDClient] = {
+    try {
+      Some(new NonBlockingStatsDClient("spark", "localhost", 8125)) 
+    } catch {
+      case ex: StatsDClientException => None
+      case ex: Exception => throw ex
+    }
+  }
   
-  def taskBaseMetrics(e: SparkListenerTaskEnd): Unit = {
+  def taskBaseMetrics(statsd: NonBlockingStatsDClient, e: SparkListenerTaskEnd): Unit = {
     statsd.incrementCounter("firehose.taskEnded")
     statsd.recordExecutionTime("firehose.taskDuration", e.taskInfo.duration)
     if (!e.taskInfo.successful) statsd.incrementCounter("firehose.taskFailed")
@@ -30,71 +37,73 @@ class DatadogRelay(conf: SparkConf) extends SparkFirehoseListener {
     statsd.count("firehose.taskBytesSentToDriver", e.taskMetrics.resultSize)
   }
   
-  def taskInputMetrics(metrics: InputMetrics): Unit = {
+  def taskInputMetrics(statsd: NonBlockingStatsDClient, metrics: InputMetrics): Unit = {
     statsd.count("firehose.taskInputBytesRead", metrics.bytesRead)
     statsd.count("firehose.taskInputRecordsRead", metrics.recordsRead)
   }
   
-  def taskShuffleReadMetrics(metrics: ShuffleReadMetrics): Unit = {
+  def taskShuffleReadMetrics(statsd: NonBlockingStatsDClient, metrics: ShuffleReadMetrics): Unit = {
     statsd.count("firehose.taskShuffleBytesRead", metrics.totalBytesRead)
     statsd.count("firehose.taskShuffleRecordsRead", metrics.recordsRead)
   }
   
-  def taskShuffleWriteMetrics(metrics: ShuffleWriteMetrics): Unit = {
+  def taskShuffleWriteMetrics(statsd: NonBlockingStatsDClient, metrics: ShuffleWriteMetrics): Unit = {
     statsd.count("firehose.taskShuffleBytesWritten", metrics.shuffleBytesWritten)
     statsd.count("firehose.taskShuffleRecordsWritten", metrics.shuffleRecordsWritten)
   }
   
-  def taskOutputMetrics(metrics: OutputMetrics): Unit = {
+  def taskOutputMetrics(statsd: NonBlockingStatsDClient, metrics: OutputMetrics): Unit = {
     statsd.count("firehose.taskOutputBytesWritten", metrics.bytesWritten)
     statsd.count("firehose.taskOutputRecordsWritten", metrics.recordsWritten)
   }
   
   override def onEvent(event: SparkListenerEvent): Unit = {
-    event match {
-      case e: SparkListenerApplicationStart =>
-        statsd.incrementCounter("firehose.applicationStarted")
-      case e: SparkListenerApplicationEnd =>
-        statsd.incrementCounter("firehose.applicationEnded")
-      case e: SparkListenerJobStart =>
-        statsd.incrementCounter("firehose.jobStarted")
-      case e: SparkListenerJobEnd =>
-        statsd.incrementCounter("firehose.jobEnded")
-      case e: SparkListenerStageSubmitted =>
-        e.stageInfo.submissionTime.foreach { _ =>
-          statsd.incrementCounter("firehose.stageStarted")
-        }
-      case e: SparkListenerStageCompleted =>
-        e.stageInfo.completionTime.foreach { completionTime =>
-          statsd.incrementCounter("firehose.stageEnded")
-          e.stageInfo.submissionTime.foreach { submissionTime =>
-            statsd.recordExecutionTime("firehose.stageProcessingTime", completionTime - submissionTime)
+    statsdOption.foreach { statsd =>
+      event match {
+        case e: SparkListenerApplicationStart =>
+          statsd.incrementCounter("firehose.applicationStarted")
+        case e: SparkListenerApplicationEnd =>
+          statsd.incrementCounter("firehose.applicationEnded")
+        case e: SparkListenerJobStart =>
+          statsd.incrementCounter("firehose.jobStarted")
+        case e: SparkListenerJobEnd =>
+          statsd.incrementCounter("firehose.jobEnded")
+        case e: SparkListenerStageSubmitted =>
+          e.stageInfo.submissionTime.foreach { _ =>
+            statsd.incrementCounter("firehose.stageStarted")
           }
-          statsd.recordGaugeValue("firehose.stageRetryCount", e.stageInfo.attemptId)
-          e.stageInfo.failureReason.foreach { _ =>
-            statsd.incrementCounter("firehose.stageFailed")
+        case e: SparkListenerStageCompleted =>
+          e.stageInfo.completionTime.foreach { completionTime =>
+            statsd.incrementCounter("firehose.stageEnded")
+            e.stageInfo.submissionTime.foreach { submissionTime =>
+              statsd.recordExecutionTime("firehose.stageProcessingTime", completionTime - submissionTime)
+            }
+            statsd.recordGaugeValue("firehose.stageRetryCount", e.stageInfo.attemptId)
+            e.stageInfo.failureReason.foreach { _ =>
+              statsd.incrementCounter("firehose.stageFailed")
+            }
           }
-        }
-      case e: SparkListenerTaskStart =>
-        statsd.incrementCounter("firehose.taskStarted")
-        statsd.recordGaugeValue("firehose.taskRetryCount", e.taskInfo.attempt)
-      case e: SparkListenerTaskEnd =>
-        taskBaseMetrics(e)
-        e.taskMetrics.inputMetrics.foreach { m => taskInputMetrics(m) }
-        e.taskMetrics.shuffleReadMetrics.foreach { m => taskShuffleReadMetrics(m) }
-        e.taskMetrics.shuffleWriteMetrics.foreach { m => taskShuffleWriteMetrics(m) }
-        e.taskMetrics.outputMetrics.foreach { m => taskOutputMetrics(m) }
-      case e: SparkListenerExecutorAdded =>
-        statsd.incrementCounter("firehose.executorAdded")
-        statsd.count("firehose.executorCoresAdded", e.executorInfo.totalCores)
-      case e: SparkListenerExecutorRemoved =>
-        statsd.incrementCounter("firehose.executorRemoved")
-      case e: SparkListenerBlockManagerAdded =>
-        statsd.incrementCounter("firehose.blockManagerAdded")
-      case e: SparkListenerBlockManagerRemoved =>
-        statsd.incrementCounter("firehose.blockManagerRemoved")
-      case _ =>
-        None
+        case e: SparkListenerTaskStart =>
+          statsd.incrementCounter("firehose.taskStarted")
+          statsd.recordGaugeValue("firehose.taskRetryCount", e.taskInfo.attempt)
+        case e: SparkListenerTaskEnd =>
+          taskBaseMetrics(statsd, e)
+          e.taskMetrics.inputMetrics.foreach { m => taskInputMetrics(statsd, m) }
+          e.taskMetrics.shuffleReadMetrics.foreach { m => taskShuffleReadMetrics(statsd, m) }
+          e.taskMetrics.shuffleWriteMetrics.foreach { m => taskShuffleWriteMetrics(statsd, m) }
+          e.taskMetrics.outputMetrics.foreach { m => taskOutputMetrics(statsd, m) }
+        case e: SparkListenerExecutorAdded =>
+          statsd.incrementCounter("firehose.executorAdded")
+          statsd.count("firehose.executorCoresAdded", e.executorInfo.totalCores)
+        case e: SparkListenerExecutorRemoved =>
+          statsd.incrementCounter("firehose.executorRemoved")
+        case e: SparkListenerBlockManagerAdded =>
+          statsd.incrementCounter("firehose.blockManagerAdded")
+        case e: SparkListenerBlockManagerRemoved =>
+          statsd.incrementCounter("firehose.blockManagerRemoved")
+        case _ =>
+          None
+      }
     }
   }
 }
